74f160080a4a488d54f6187cb4280608cce1b2c7
SUGIMOTO Norimitsu <dictoss@live.jp>
2014-08-23 15:10:35 +0900

    add entry wsgi file.

---
+++ chimeproj/wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+path = os.path.realpath(os.path.dirname(__file__))
+if not path in sys.path:
+    sys.path.append(path)
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chimeproj.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
